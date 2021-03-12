e7aae157df46d6e8efd674b46a5d53cda5b5401f
Malachi Spencer <m.spencer@live.co.uk>
2021-03-12 13:59:32 +0000

    Explained bitwise AND

---
+++ bitwise-and-of-numbers-range.js
@@ -0,0 +1,23 @@
+/*
+Leetcode. 12/03/2021. Given two integers left and right that represent a range, return the bitwise AND of all numbers in this range,
+inclusive.
+
+Bitwise AND
+A bitwise AND is a binary operation that takes two equal length binary representations and performs the logical AND operation on each pair
+of the corresponding bits, which is the equivalent of multiplying them. If both bits in the compared position are 1, the resultant bit is
+1, otherwise, the resultant bit is 0. 
+
+5 AND 6:
+5 = 00000101
+6 = 00000110
+R = 00000100 = 4
+
+6 AND 7:
+6 = 00000110
+7 = 00000111
+R = 00000100 = 4
+*/
+
+function rangeBitwiseAND(left, right) {
+
+}
